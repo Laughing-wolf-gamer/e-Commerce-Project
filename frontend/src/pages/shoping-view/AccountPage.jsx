de805ffc92614538d004c0d72b73eeac6af45766
Abhishek sinha <laughingwolfgamer@gmail.com>
2024-12-06 16:41:23 +0530

    Update 16

--- frontend/src/pages/shoping-view/AccountPage.jsx
+++ frontend/src/pages/shoping-view/AccountPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import accImg from '../../assets/account.jpg'
 const ShoppingAccountPage = () => {
     return (
         <div className='flex flex-col'>
-            <div className='relative h-[300px] w-full'>
-                
+            <div className='relative h-[300px] w-full overflow-hidden'>
+                    <img
+                        width={'1600'}
+                        height={'300'}
+                        style={{aspectRatio:"1600/300",objectFit:'cover'}}
+                        src={accImg}
+                        alt='accountImg'
+                        className='h-full w-full object-cover object-center'
+                    />
             </div>
         </div>
     )
